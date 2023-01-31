85e07113368ed90dc8aaa3881217f62ea1f588ea
StripedBear <stripedbear@tutanota.com>
2023-01-31 15:25:49 +0200

    Second commit

--- telegram.py
+++ telegram.py
@@ -6,4 +6,4 @@ config = configparser.ConfigParser()
 config.read_file(codecs.open("config.ini", 'r', 'utf8'))
 client = TelegramClient(config['Telegram']['number'], int(config['Telegram']['api_id']), config['Telegram']['api_hash'])
 with client:
-    print('Готово')
+    print('Ready')
